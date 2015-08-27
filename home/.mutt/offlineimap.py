ddf7e4d20358942fa78157280fb2ce9a78fac60c
Jeffrey Horn <jeffh@generalassemb.ly>
2015-08-26 20:33:59 -0400

    Add name translator helpers

--- home/.mutt/offlineimap.py
+++ home/.mutt/offlineimap.py
@@ -14,3 +14,53 @@ def get_keychain_pass(account=None, server=None):
             if l.startswith('password: ')][0]
 
     return re.match(r'password: "(.*)"', outtext).group(1)
+
+# nametrans helpers
+l2r_mapping = {
+  'inbox': 'INBOX'
+, 'drafts': 'INBOX.Drafts'
+, 'junk': 'INBOX.Junk Mail'
+, 'sent': 'INBOX.Sent Items'
+, 'trash': 'INBOX.Trash'
+, 'sane/blackhole': 'INBOX.+SaneBlackHole'
+, 'sane/bulk': 'INBOX.+SaneBulk'
+, 'sane/community': 'INBOX.+SaneCommunity'
+, 'sane/date': 'INBOX.+SaneDate'
+, 'sane/deals': 'INBOX.+SaneDeals'
+, 'sane/github': 'INBOX.+SaneGitHub'
+, 'sane/later': 'INBOX.+SaneLater'
+, 'sane/learning': 'INBOX.+SaneLearning'
+, 'sane/news': 'INBOX.+SaneNews'
+, 'sane/social': 'INBOX.+SaneSocial'
+, 'archive/coupons': 'INBOX.Archive.Coupons'
+, 'archive/hold': 'INBOX.Archive.Hold'
+, 'archive/vacation': 'INBOX.Archive.Vacation'
+, 'archive': 'INBOX.Archive'
+}
+
+r2l_mapping = { val: key for key, val in l2r_mapping.items() }
+
+def nt_remote(folder):
+    try:
+        return r2l_mapping[folder]
+    except:
+        return folder
+
+def nt_local(folder):
+    try:
+        return l2r_mapping[folder]
+    except:
+        return folder
+
+
+def is_included(folder):
+  result = True
+
+  for pattern in exclusion_patterns:
+    result = result and (re.search(pattern, folder) == None)
+
+  return result
+
+exclusion_patterns = [
+  'RESTORED'
+]
